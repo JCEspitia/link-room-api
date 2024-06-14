13f7199b6651cf6d34ce56063ea610baa4f6bd23
Camilo Espitia <jcespitia100@gmail.com>
2024-06-13 21:44:29 -0500

    Fix deployment

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
- echo "BUILD START"
+echo "BUILD START"
+pip install --upgrade pip
 python3.12 -m pip install -r requirements.txt
 
 echo "Make migrations..."
+
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
 
 python3.12 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD END"
