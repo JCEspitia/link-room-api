bcd13dd8048e4b71f067f423eb86a1991a20ff4f
Camilo Espitia <jcespitia100@gmail.com>
2024-06-13 21:36:54 -0500

    Fix deployment

--- link_room/settings.py
+++ link_room/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'link_room.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config()
 }
 
 # Password validation
