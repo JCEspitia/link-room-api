a3c298eb550e088fcc0d77e367eea82b1d2f0420
Camilo Espitia <jcespitia100@gmail.com>
2024-06-13 22:07:28 -0500

    Fix compatibility with client

--- link_room/settings.py
+++ link_room/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['*']
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
